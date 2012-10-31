b5766aa0ffeda103ac0e480f95f1e747a80450e0
Phil <philipp.koller@live.de>
2012-10-31 12:57:46 +0100

    Creating Entities

---
+++ DonkeyKong/GameStateManagementSample/Entities/PlayerEntity.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameStateManagementSample.Entities
+{
+    class PlayerEntity : AnimationEntity
+    {
+    }
+}
